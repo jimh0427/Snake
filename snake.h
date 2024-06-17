2c84dec9ab2c05dd58937b87995ce4ca9a17e05c
jimh0427 <133951624+jimh0427@users.noreply.github.com>
2024-06-17 18:08:17 +0900

    Update snake.h

--- snake.h
+++ snake.h
@@ -1,9 +1,9 @@
 #ifndef _NCURSESPROJECT_
 #define _NCURSESPROJECT_
 #include <ncursesw/curses.h>
-#include <utility>
 #include <string>
 #include <iostream>
+
 
 class body{
 public:
@@ -25,10 +25,7 @@ public:
 };
 
 void remove(WINDOW *win, int &x, int &y, int gateList[21][21]);//x,y 좌표의 색 지우기
-// void removeGate(WINDOW *win, int &x, int &y, int gateList[21][21]);//x,y 좌표의 색 지우기
 void move(WINDOW *win, int &x, int &y, int &color_type);//x,y좌표에 색 칠하기
-void moveGate(WINDOW *win, int &x, int &y);//x,y좌표에 색 칠하기
-
 void memory(body **p, int x, int y, body *n);//body 객체 생성해서 head.next에 연결
 
 
